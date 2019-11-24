0cd7b206f351d12cd6d91f4a632c9afab70f3b91
mittelgrau <mittelgrau.dev@gmail.com>
2019-11-24 01:13:58 +0100

    fixed postcss order && updated all depedencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,10 @@
-var tailwindcss = require('tailwindcss');
-
 module.exports = {
     plugins: [
-        tailwindcss(),
-        require('autoprefixer'),
+        require('postcss-import'),
+        require('tailwindcss'),
         require('postcss-nested'),
-        require('postcss-preset-env')
+        require('autoprefixer'),
+        require('postcss-preset-env'),
+        require('cssnano')
     ]
 };
