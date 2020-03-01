c5ea1196ff7459110b041566bf602d74336cd931
mittelgrau <mittelgrau.dev@gmail.com>
2020-03-01 14:56:11 +0100

    update dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,59 @@
+const pluginLocalRespimg = require('eleventy-plugin-local-respimg');
 const htmlmin = require("html-minifier");
+
 module.exports = function (config) {
     // Add a filter using the Config API
     config.addLiquidFilter("makeUppercase", function (value) {});
     config.addPassthroughCopy("static");
 
-    config.addTransform("htmlmin", function(content, outputPath) {
+    config.addTransform("htmlmin", function (content, outputPath) {
         console.log("this is the outputPath" + outputPath)
-        if(outputPath.endsWith(".html") ) {
-          let minified = htmlmin.minify(content, {
-            useShortDoctype: true,
-            removeComments: true,
-            collapseWhitespace: true
-          });
-          return minified;
+        if (outputPath.endsWith(".html")) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+            return minified;
         }
 
         return content;
-      });
+    });
+
+    config.addPlugin(pluginLocalRespimg) {
+        folders: {
+            source: 'src',
+            output: 'public',
+        },
+        images: {
+            resize: {
+                min: 250,
+                max: 1500,
+                step: 150
+            },
+            sizes: 100 vw,
+            lazy: true,
+            additional: [
+                'images/icons/**/*'
+            ],
+            watch: {
+                src: 'images/**/*',
+            },
+            pngquant: {
+
+            },
+            mozjpeg: {
+
+            },
+            svgo {
+
+            },
+            gifresize: {
+
+            },
+            webp: {},
+            gifwebp: {}
+        }
+
+    }
 };
